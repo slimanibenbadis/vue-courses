6b6584745422d5bc3cd7fa29a3a3d6afe53d5abd
slimanibenbadis <slimani.b@gmail.com>
2022-03-13 22:53:50 +0100

    Using Lifecycle Hooks

--- beyond-fundamentals-1/app.js
+++ beyond-fundamentals-1/app.js
@@ -3,6 +3,30 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+    console.log('beforeCreate() function called!', this.message);
+  },
+  created() {
+    console.log('created() function called!', this.message);
+  },
+  beforeMount() {
+    console.log('beforeMount() function called!', this.$el);
+  },
+  mounted() {
+    console.log('mounted() function called!', this.$el);
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate() function called!');
+  },
+  updated() {
+    console.log('updated() function called!');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount() function called!');
+  },
+  unmounted() {
+    console.log('unmounted() function called!');
   }
 })
 
